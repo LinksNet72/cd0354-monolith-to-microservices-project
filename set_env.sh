012dcd941d6116941abe81933da8280d18f71502
LinksNet72 <gakolade@gmail.com>
2022-07-12 18:21:48 +0100

    Remove setenv secrets

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=kolade
-export POSTGRES_PASSWORD=koladeuser
-export POSTGRES_HOST=udagram3-koladedb-dev.cilvfd8qg8ng.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=udagramdb
-export AWS_BUCKET=arn:aws:s3:::udagram3-koladebucket-dev
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=
+export AWS_BUCKET=
+export AWS_REGION=
+export AWS_PROFILE=
+export JWT_SECRET=
+export URL=
